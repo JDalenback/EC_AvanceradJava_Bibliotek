9a8aff77ca703d5c2f48684e796c86b87fa466db
PalmquistS <71629477+PalmquistS@users.noreply.github.com>
2021-01-27 15:59:04 +0100

    created Book and BookTracker class content, and List listOfAllBooks

--- src/models/BookTracker.java
+++ src/models/BookTracker.java
@@ -1,4 +1,48 @@
 package models;
 
-public class BookTracker {
+import java.io.Serializable;
+
+public class BookTracker implements Serializable {
+    private boolean isAvailable;
+    private int dateOfBorrow;
+    private String userThatBorrowed;
+
+    public BookTracker() {
+        this.isAvailable = true;
+        this.dateOfBorrow = 0;
+        this.userThatBorrowed = null;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    public int getDateOfBorrow() {
+        return dateOfBorrow;
+    }
+
+    public void setDateOfBorrow(int dateOfBorrow) {
+        this.dateOfBorrow = dateOfBorrow;
+    }
+
+    public String getUserThatBorrowed() {
+        return userThatBorrowed;
+    }
+
+    public void setUserThatBorrowed(String userThatBorrowed) {
+        this.userThatBorrowed = userThatBorrowed;
+    }
+
+    @Override
+    public String toString() {
+        return "BookTracker{" +
+                "isAvailable=" + isAvailable +
+                ", dateOfBorrow=" + dateOfBorrow +
+                ", userThatBorrowed='" + userThatBorrowed + '\'' +
+                '}';
+    }
 }
