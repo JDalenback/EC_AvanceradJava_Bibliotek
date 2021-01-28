fc1e3d862f8f96ac2b3dcad87defb4d0341dd229
JonasHolmkvist <jonas_holmkvist@hotmail.se>
2021-01-28 09:29:57 +0100

    I have created the content in User and Librarian. I also created Visitor

--- src/models/User.java
+++ src/models/User.java
@@ -1,4 +1,52 @@
 package models;
 
-public class User {
+import java.util.HashMap;
+
+abstract class User {
+
+    private String name;
+    private String userID;
+    private HashMap<String, Book> myBookList = new HashMap<>();
+
+
+    public User(String name, String userID) {
+        this.name = name;
+        this.userID = userID;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public HashMap<String, Book> getMyBookList() {
+        return myBookList;
+    }
+
+    public void setMyBookList(HashMap<String, Book> myBookList) {
+        this.myBookList = myBookList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", userID='" + userID + '\'' +
+                ", myBookList=" + myBookList +
+                '}';
+    }
+
+
 }
