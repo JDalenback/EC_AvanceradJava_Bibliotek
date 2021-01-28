9b8a4df7a0891e7bf980b10db9ba455b3b1668c9
Johannes Dalenbäck <musicunlim@gmail.com>
2021-01-28 10:43:47 +0100

    Added functionality sto show all books in the library and ot search for book based on title, author or isbn

--- src/models/Book.java
+++ src/models/Book.java
@@ -3,26 +3,26 @@ package models;
 import java.io.Serializable;
 
 public class Book implements Serializable {
-    private String bookTitle;
+    private String title;
     private String author;
     private String isbn;
     private String description;
     private BookTracker bookTracker;
 
-    public Book(String bookTitle, String author, String isbn, String description, BookTracker bookTracker) {
-        this.bookTitle = bookTitle;
+    public Book(String title, String author, String isbn, String description, BookTracker bookTracker) {
+        this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.description = description;
         this.bookTracker = bookTracker;
     }
 
-    public String getBookTitle() {
-        return bookTitle;
+    public String getTitle() {
+        return title;
     }
 
-    public void setBookTitle(String bookTitle) {
-        this.bookTitle = bookTitle;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getAuthor() {
@@ -59,12 +59,6 @@ public class Book implements Serializable {
 
     @Override
     public String toString() {
-        return "Book{" +
-                "bookTitle='" + bookTitle + '\'' +
-                ", author='" + author + '\'' +
-                ", isbn=" + isbn +
-                ", description='" + description + '\'' +
-                ", bookTracker=" + bookTracker +
-                '}';
+        return String.format("Title: %s. Author: %s. ISBN: %s", title, author, isbn);
     }
 }
